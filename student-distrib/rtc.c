3b11cbfe3e32ad0ba9dabc503f6170aea957ee1d
Salunke <salunke4@illinois.edu>
2023-03-20 21:34:17 -0500

    rtc

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -6,10 +6,12 @@ uint32_t rtc_counter = 0;
 
 void rtc_init(){
     
-    disable_irq(RTC_IRQ);		// important that no interrupts happen (perform a CLI)
-    outb(0x8A, 0x70);	// select Status Register A, and disable NMI (by setting the 0x80 bit)
-    outb(0x20, 0x71);	// write to CMOS/RTC RAM
-    enable_irq(RTC_IRQ);		// (perform an STI) and reenable NMI if you wish
+    disable_irq(RTC_IRQ);			// disable interrupts
+    outb(0x8B, 0x70);		// select register B, and disable NMI
+    char prev = inb(0x71);	// read the current value of register B
+    outb(0x8B, 0x70);		// set the index again (a read will reset the index to register D)
+    outb(prev | 0x40, 0x71);	// write the previous value ORed with 0x40. This turns on bit 6 of register B
+    enable_irq(RTC_IRQ);
     
 }
 
@@ -19,11 +21,6 @@ void rtc_handler(){
     
     outb(0x0C, 0x70);	// select register C
     inb(0x71);		// just throw away contents
-    
-    rtc_counter--;
-    if(rtc_counter==0){
-        rtc_counter = 512
-    }
     
     // end interrupt
     send_eoi(RTC_IRQ);
