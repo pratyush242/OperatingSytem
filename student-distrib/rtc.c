0c42a121559a228b3268c7d5b92da54a6059631f
Salunke <salunke4@illinois.edu>
2023-03-20 21:22:54 -0500

    rtc

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -2,13 +2,14 @@
 #include "lib.h"
 
 
-int rtc_counter = 0;
+uint32_t rtc_counter = 0;
 
 void rtc_init(){
+    
     disable_irq(RTC_IRQ);		// important that no interrupts happen (perform a CLI)
-    outb(0x70, 0x8A);	// select Status Register A, and disable NMI (by setting the 0x80 bit)
-    outb(0x71, 0x20);	// write to CMOS/RTC RAM
-    enable_ints(RTC_IRQ);		// (perform an STI) and reenable NMI if you wish
+    outb(0x8A, 0x70);	// select Status Register A, and disable NMI (by setting the 0x80 bit)
+    outb(0x20, 0x71);	// write to CMOS/RTC RAM
+    enable_irq(RTC_IRQ);		// (perform an STI) and reenable NMI if you wish
     
 }
 
@@ -16,7 +17,7 @@ void rtc_init(){
 
 void rtc_handler(){
     
-    outb(0x70, 0x0C);	// select register C
+    outb(0x0C, 0x70);	// select register C
     inb(0x71);		// just throw away contents
     
     rtc_counter--;
