45df9b33b849ed4de84a3d85f4e3fae7980e27b0
kochhar4 <kochhar4@illinois.edu>
2023-03-25 23:06:37 -0500

    Update student-distrib/rtc.c

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -2,37 +2,29 @@
 #include "lib.h"
 #include "i8259.h"
 
-//rtc initializer
 
-void rtc_init(){
-    
-    //disable_irq(RTC_IRQ);			// disable interrupts
+//rtc initializer
+void rtc_init()
+{
+    cli();	
     outb(0x8B, 0x70);		// select register B, and disable NMI
     char prev = inb(0x71);	// read the current value of register B
     outb(0x8B, 0x70);		// set the index again (a read will reset the index to register D)
-    outb(prev | 0x40, 0x71);	// write the previous value ORed with 0x40. This turns on bit 6 of register B
+    outb(prev  | 0x40, 0x71);	// write the previous value ORed with 0x40. This turns on bit 6 of register B
     enable_irq(RTC_IRQ);
-    
-}
-
-
-//rtc handler
-void rtc_handler(){
-
-    cli();
-    
-    outb(0x0C, 0x70);	// select register C
-    inb(0x71);		// just throw away contents
-    //test_interrupts();
-    // end interrupt
+    enable_irq(SLAVE_IRQ); 
     sti();
-    send_eoi(RTC_IRQ);
-   
+    
 }
+//rtc handler
+void rtc_handler() {
+    /* mask the interrupts*/
+    cli();
+    //test_interrupts();
+    outb(0x0C, 0x70); // select register C
+    inb(0x71); // just throw away contents
+    // end interrupt
+    send_eoi(RTC_IRQ);
     
-    
-
-
-
-
-
+    sti();
+}
