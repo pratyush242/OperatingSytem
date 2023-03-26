ed82b4f8cf1858a8d84a4129cba53cb1914ff30b
kochhar4 <kochhar4@illinois.edu>
2023-03-25 23:08:24 -0500

    Update student-distrib/rtc.h

--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -1,9 +1,10 @@
 #ifndef _RTC_H
 #define _RTC_H
 
-#define RTC_IRQ 8       //the irq num for keybaord 
+#define RTC_IRQ 8  
+#define SLAVE_IRQ 2
 
 extern void rtc_init();
 extern void rtc_handler();
 
-#endif
+#endif 
