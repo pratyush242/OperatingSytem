668af9e0a5536762af81f9d6d67201f7b3e51ced
salunke4 <salunke4@illinois.edu>
2023-04-10 22:40:18 -0500

    Replace systemcalls.c

--- student-distrib/systemcalls.c
+++ student-distrib/systemcalls.c
@@ -77,102 +77,24 @@ int32_t execute(const uint8_t* command){
     if(prog_count >= 8){
         return 0;
     } 
-
     
-
-    
-    if(strncmp((int8_t*) command, (int8_t*) "shell", 5) == 0){
-        pid = prog_count = 0;
-    }
-    
-    
-
     /* parsing arguments */
 
-    uint8_t argv[16][128];    
-    uint8_t cmd[10];
-    int32_t cases;
-
-    int32_t i,j, command_length;
-    int32_t start = 0;
-    int32_t end = 0;
-
+    uint8_t argv[32];    
     
-    for(i = 0; i < 10; i++){
-        cmd[i] = '\0';
-    }
-
-    for(i = 0; i < 16; i++){
-        for(j = 0; j < 128; j++){
-            argv[i][j] = '\0';
-        }
-    }
-
-    
-    while (command[start] == ' '){
-        start++; 
-        end++;
-    }
-
    
-    while (command[end] != ' ' && command[end] != '\0' && command[end] != 0x0A){ 
-        end++; 
-    } 
+    int32_t i;
 
-    for (i = 0; i < (end - start); i++){
-        cmd[i] = command[i + start];
-    } 
-        
-
-    command_length = strlen((int8_t*) command);
-
-    
-    for(j = 0 ; end <= command_length ; end++, j++){
-        start = end;
-        
-        while (command[start] == ' '){
-            start++;
-            end++;
-        }
-
-        while (command[end] != ' ' && command[end] != '\0' && command[end] != 0x0A){
-            end++;
-        }
-
-        for (i = 0; i < (end - start); i++){
-            argv[j][i] = command[i + start];
-        }
-        
-        argv[j][end - start] = '\0';
-    }
-
-    if (strncmp("exit", (int8_t*) cmd, 4) == 0){
-        cases = 1;
-    }
-
-    else{
-        cases = 0;
-    }
-    
-    
-    sti();
-    
-    switch(cases){
-        case 0:
+    for (i = 0; i < 32; i++){
+        if (command[i] == ' '){
             break;
-        case 1:
-            asm volatile(
-                "pushl  $0;"
-                "pushl  $0;"
-                "pushl  %%eax;"
-                "call halt;"
-                :
-                );
-            break;
-        case -1:
-            break;
+        } 
+        else{
+            argv[i] = command[i];
+        } 
     }
-
+                                    
+    
     /* executable check */
     dentry_t dentry;
     uint8_t  buf[4];
@@ -196,7 +118,7 @@ int32_t execute(const uint8_t* command){
 
     read_data(dentry.inode_num, 24, buf, 4);
 
-    entry_point = *((uint32_t*) buf);
+   
 
     
     
