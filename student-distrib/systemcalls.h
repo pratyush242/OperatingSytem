9823391fbb5515c6426b64834c6b9b4caf6e9314
pthakur3 <pthakur3@illinois.edu>
2023-04-13 16:28:55 -0500

    Replace systemcalls.h

--- student-distrib/systemcalls.h
+++ student-distrib/systemcalls.h
@@ -5,8 +5,11 @@
 #include "filesys.h"
 #include "terminal.h"
 #include "rtc.h"
+#include "lib.h"
 
 //pcb_t* pcb_adress(uint32_t pid);
+
+extern void sysCallPaging(uint32_t pid);
 
 int32_t sys_open(const char* fname);
 
@@ -20,23 +23,23 @@ int32_t system_execute(const uint8_t* cmd);
 
 int32_t halt(uint8_t status);
 
-uint32_t null_open();
+int32_t null_open();
 
-uint32_t null_read();
+int32_t null_read();
 
-uint32_t null_write();
+int32_t null_write();
 
-uint32_t null_close();
+int32_t null_close();
 
-
+extern int32_t contextSwitch(uint32_t buf,uint32_t espArg);
 
 
 /* struct for fops */
 typedef struct fops_table_t {
-    int32_t (*read)(int32_t fd, void* buf, int32_t nbytes);
-    int32_t (*write)(int32_t fd, void* buf, int32_t nbytes);
-    int32_t (*open)(const char* fname);
-    int32_t (*close)(int32_t fd);
+   int32_t (*sys_read)(int32_t fd, void* buf, int32_t nbytes);
+   int32_t (*sys_write)(int32_t fd, void* buf, int32_t nbytes);
+   int32_t (*sys_open)(const char* fname);
+  int32_t (*sys_close)(int32_t fd);
 } fops_table_t;
 
 typedef struct file_descriptor_t {
@@ -49,46 +52,46 @@ typedef struct file_descriptor_t {
 // pointer pointing to fd array
 file_descriptor_t* file_descriptor_array;
 
-static fops_table_t fopsarray[6];
-void file_op_table_init()
-{
-    // init rtc operation table 
-    fopsarray[0].open  = rtc_open;
-    fopsarray[0].close = rtc_close;
-    fopsarray[0].read  = rtc_read;
-    fopsarray[0].write = rtc_write;
 
-    // init dir operation table 
-    // fopsarray[1].open  = open_directory ;
-    // fopsarray[1].close = close_directory;
-    fopsarray[1].read  = read_directory ;
-    // fopsarray[1].write = write_directory;
+// void file_op_table_init()
+// {
+//     // init rtc operation table 
+//     fopsarray[0].open  = rtc_open;
+//     fopsarray[0].close = rtc_close;
+//     fopsarray[0].read  = rtc_read;
+//     fopsarray[0].write = rtc_write;
 
-    // init file operation table 
-    fopsarray[2].open  = open;
-    fopsarray[2].close = close;
-    fopsarray[2].read  = read_file;
-    fopsarray[2].write = write;
+//     // init dir operation table 
+//     // fopsarray[1].open  = open_directory ;
+//     // fopsarray[1].close = close_directory;
+//     fopsarray[1].read  = read_directory ;
+//     // fopsarray[1].write = write_directory;
 
-    // stdin
-    fopsarray[3].open  = terminal_open;
-    fopsarray[3].close = terminal_close;
-    fopsarray[3].read  = terminal_read;        // would be 1 cuz read only
-    fopsarray[3].write = terminal_write;         // would be 0 cuz read only
+//     // init file operation table 
+//     fopsarray[2].open  = open;
+//     fopsarray[2].close = close;
+//     fopsarray[2].read  = read_file;
+//     fopsarray[2].write = write;
 
-    //stdout
-    fopsarray[4].open  = terminal_open;
-    fopsarray[4].close = terminal_close;
-    fopsarray[4].read  = terminal_read;          // would be 1 cuz write only
-    fopsarray[4].write = terminal_write;          // would be 1 cuz write only
+//     // stdin
+//     fopsarray[3].open  = terminal_open;
+//     fopsarray[3].close = terminal_close;
+//     fopsarray[3].read  = terminal_read;        // would be 1 cuz read only
+//     fopsarray[3].write = terminal_write;         // would be 0 cuz read only
 
-    //null
+//     //stdout
+//     fopsarray[4].open  = terminal_open;
+//     fopsarray[4].close = terminal_close;
+//     fopsarray[4].read  = terminal_read;          // would be 1 cuz write only
+//     fopsarray[4].write = terminal_write;          // would be 1 cuz write only
+
+//     //null
     
-    fopsarray[5].open  = -1;
-    fopsarray[5].close = -1;
-    fopsarray[5].read  = -1;         
-    fopsarray[5].write = -1;         
-}
+//     fopsarray[5].open  = -1;
+//     fopsarray[5].close = -1;
+//     fopsarray[5].read  = -1;         
+//     fopsarray[5].write = -1;         
+// }
 
 typedef struct pcb_t {
     uint32_t pid;    // file operator table 
