5767c3be666063135e5963fbd146532f47a0a061
pthakur3 <pthakur3@illinois.edu>
2023-04-13 16:18:14 -0500

    Replace filesys.h

--- student-distrib/filesys.h
+++ student-distrib/filesys.h
@@ -54,7 +54,7 @@ uint32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry);
 uint32_t read_dentry_by_index(uint32_t index,dentry_t* dentry);
 uint32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
 int write();
-uint32_t read_file(const uint8_t* fname, uint8_t* buf, uint32_t length);
-uint32_t read_directory( uint8_t* buf);
+uint32_t read_file(int32_t  fname,void* buf, int32_t length);
+uint32_t read_directory(int32_t fd, void* buf, int32_t nbytes);
 
 #endif
