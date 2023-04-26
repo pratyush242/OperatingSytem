d61bb77b97f3cb07b95e44c6ad228c8b782286fd
salunke4 <salunke4@illinois.edu>
2023-04-25 20:58:52 -0500

    Add new file

---
+++ pit.c
@@ -0,0 +1,28 @@
+#include "pit.h"
+#include "lib.h"
+
+#include "i8259.h"
+
+
+void pit_init()
+{   
+
+    
+
+    int divisor = 1193180 / PIT_HZ;       /* Calculate our divisor */
+    outb(0x36, 0x43);             /* Set our command byte 0x36 */
+    outb(divisor & 0xFF, 0x40);   /* Set low byte of divisor */
+    outb(divisor >> 8, 0x40);     /* Set high byte of divisor */
+    enable_irq(0x20);
+  
+    
+}
+
+void pit_init()
+{   
+    
+    enable_irq(0x20);
+    switch(); /* video memory and context switch */
+  
+    
+}
