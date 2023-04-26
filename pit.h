48abaeb3875242cdfe6034ceddb7bf43565e6028
salunke4 <salunke4@illinois.edu>
2023-04-25 20:59:17 -0500

    Add new file

---
+++ pit.h
@@ -0,0 +1,14 @@
+#ifndef _PIT_H
+#define _PIT_H
+
+
+
+
+/*Constants to set the PIT frequency*/
+#define PIT_HZ        50       / * frequency in Hz
+
+
+void pit_init();
+void pit_handler();
+
+#endif
